ed0740dfc9edb22c62007e96dddd39a613cdc3e6
Robert Uivarosi <robert.cristian.uivarosi@gmail.com>
2016-07-07 18:40:50 +0300

    Add county functionality implemented.

--- application/controllers/CountyController.php
+++ application/controllers/CountyController.php
@@ -13,7 +13,34 @@ class CountyController extends Zend_Controller_Action
         $county = new Application_Model_CountyMapper();
         $this->view->entries = $county->fetchAll();
     }
-
-
+    
+    public function addcountyAction()
+    {
+        $county = new Application_Model_DbTable_County();
+        $posted = $this->getRequest()->getPost('county');
+        
+        if ($this->getRequest()->isPost()) {
+            
+        // TEST FIELDS FOR NON-EMPTY AND LENGTH
+            if (!$county) {
+                echo '<script language="javascript">alert("County filed can not be empty.")</script>';
+            } else {
+                // REFACTOR INSERT QUERIES
+               $checkCounty = $county->fetchRow(
+                   $county->select()
+                       ->where('name = ?', $county)
+               );
+               if ($checkCounty) {
+                    echo "<script>alert('County already exists in DB.')</script>";
+               } else {
+                    $addNewCounty = $county->fetchNew();
+                    $addNewCounty->name = $posted;
+                    $addNewCounty->save();
+                    echo "<script>window.location.href='/home'</script>";
+                    $countyId = $addNewCounty->id;
+               }
+            
+           }
+        }//end of POST method check
+    }
 }
-
